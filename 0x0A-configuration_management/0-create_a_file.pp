9ec9925f44ee2418a57df74bb7d54e84fc4d512e
Rafiki Blaise <rafikiblaise@gmail.com>
2023-11-25 03:10:20 -0800

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
-#create a file in /tmp
+# create a file in /tmp
 
 file { '/tmp/school':
-  ensure  => 'file'
+  ensure  => 'file',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
