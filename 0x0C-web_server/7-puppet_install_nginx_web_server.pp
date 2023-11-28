abd4311eb7ff4cf61384c657ffe5faced13f8e79
Rafiki Blaise <rafikiblaise@gmail.com>
2023-11-28 11:55:34 -0800

    all work done

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,7 @@
+# Installs Nginx server and configures redirection
+
+exec { 'install_nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
+  unless   => 'dpkg -l | grep -qw nginx', # Ensures Nginx is not reinstalled
+}
